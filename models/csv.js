c87cfbc6483234eba892e10f600e6e958ac5805c
bhavya2116 <95634049+bhavya2116@users.noreply.github.com>
2022-07-19 22:40:05 +0530

    Update csv.js

--- models/csv.js
+++ models/csv.js
@@ -12,33 +12,6 @@ var csvSchema = new mongoose.Schema({
     product_description:{
         type:String
     }
-    // FirstName:{
-    //     type:String
-    // },
-    // LastName:{
-    //     type:String
-    // },
-    // SSN:{
-    //     type:String
-    // },
-    // Test1:{
-    //     type:Number
-    // },
-    // Test2:{
-    //     type:Number
-    // },
-    // Test3:{
-    //     type:Number
-    // },
-    // Test4:{
-    //     type:Number
-    // },
-    // Final:{
-    //     type:Number
-    // },
-    // Grade:{
-    //     type:String
-    // }
 });
 
 module.exports = mongoose.model('product',csvSchema);
